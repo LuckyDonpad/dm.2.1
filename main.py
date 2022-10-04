f2e7afc8c535e86535551214a487551ef4ab7d7a
Donpad <Superlalko31@gmail.com>
2022-10-04 21:29:46 +0300

    some fixes

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import matplotlib as plt
 from matplotlib import pyplot
 
 plt.use('TkAgg')
+"""
 for n in [5, 7, 9]:
     combs = []
     for k in range(n + 1):
@@ -13,31 +14,37 @@ for n in [5, 7, 9]:
         combs.append(combinations)
     plt.pyplot.plot(combs)
     plt.pyplot.savefig("combinations n = " + str(n) + '.jpeg')
-
+"""
+"""
 seq = []
 subsets_time = []
+n_subsets = []
 for i in range(1, 26):
+    n_subs = 0
     seq.append(i)
     start_time = time()
     for _ in subsets.subsets_generator(seq):
-        pass
+        n_subs += 1
     end_time = time()
+    n_subsets.append(n_subs)
     subsets_time.append(end_time - start_time)
     print(i, subsets_time[-1])
 plt.pyplot.plot(subsets_time)
 plt.pyplot.savefig("subsets time.jpeg")
+plt.pyplot.plot(n_subsets)
+plt.pyplot.savefig("amount subsets.jpeg")
+"""
 
-permutations_time = []
-for i in range(1, 12):
+n_permutations = []
+for i in range(1, 14):
     start_time = time()
     for _ in permutations.permutation_generator(i):
         pass
     end_time = time()
-    permutations_time.append(end_time - start_time)
-    print(i, permutations_time[-1])
-plt.pyplot.plot(permutations_time)
-plt.pyplot.savefig("permutations time.jpeg")
-
+    print(i, end_time - start_time )
+plt.pyplot.plot(n_permutations)
+plt.pyplot.savefig("n_permutations.jpeg")
+'''
 n_placements = []
 for n in [5, 7, 9]:
     for k in range(n + 1):
@@ -45,3 +52,4 @@ for n in [5, 7, 9]:
         n_placements.append(placement)
     plt.pyplot.plot(n_placements)
     plt.pyplot.savefig("n_placements n = " + str(n) + ".jpeg")
+'''
